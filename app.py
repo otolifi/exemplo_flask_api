687edc32668d8b46614af9ef6e2e2ba4fb2153be
Otoniel <otolifi@gmail.com>
2021-03-22 12:29:11 -0300

    relacao mts x mts resolvida

--- app.py
+++ app.py
@@ -11,19 +11,35 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-from models.Clientes import Cliente, Endereco
+from models.Clientes import Cliente, Endereco, Telefone, Pet, pets_has_clientes
+
+
+@app.route('/clientes')
+def get_clientes():
+    lista = dict()
+    result = Cliente.query.join(pets_has_clientes).all()
+    #print(result)
+    for x in result:
+        lista[str(x.id)] = x.serialize()
+        #print('x=', x.nome)
+        for pet in x.pets:
+            print('pet=', pet.serialize())
+            lista[str(x.id)][str(pet.id)] = pet.serialize()
+    #print(lista)
+    return lista
 
 
 @app.route('/')
 def index():
     return 'Ola, mundo'
 
-
+'''
 @app.route('/clientes', methods=['GET'])
 def get_clientes():
     clientes = Cliente.query.all()
     print(clientes)
     return jsonify([i.serialize() for i in clientes])
+    '''
 
 
 @app.route('/enderecos', methods=['GET'])
@@ -33,11 +49,33 @@ def get_enderecos():
     return jsonify([i.serialize() for i in enderecos])
 
 
+@app.route('/pets/<id>', methods=['GET'])
+def get_pets_by_id(id):
+    pets = Pet.query.filter_by(id=id)
+    print(pets)
+    return jsonify([i.serialize() for i in pets])
+
+
+@app.route('/pets', methods=['GET'])
+def get_pets():
+    pets = Pet.query.all()
+    print(pets)
+    return jsonify([i.serialize() for i in pets])
+
+
 @app.route('/endereco/<id>', methods=['GET'])
 def get_endereco(id):
     endereco = Endereco.query.filter_by(id=id).first()
     print(endereco)
     return jsonify(endereco.serialize())
+
+
+@app.route('/teste')
+def get_teste():
+    end = get_pets()
+    print(end)
+
+    return end
 
 
 
--- models/Clientes.py
+++ models/Clientes.py
@@ -1,5 +1,6 @@
 from app import db
-from flask import url_for
+from flask import url_for, session
+
 
 
 class Cliente(db.Model):
@@ -10,6 +11,12 @@ class Cliente(db.Model):
     cpf = db.Column(db.String(11), nullable=False, unique=True)
     endereco = db.relationship('Endereco')
     endereco_id = db.Column(db.Integer, db.ForeignKey('enderecos.id'), nullable=False)
+    telefone = db.relationship('Telefone')
+    telefone_id = db.Column(db.Integer, db.ForeignKey('telefones.id'), nullable=False)
+    
+    pets = db.relationship('Pet', secondary='pets_has_clientes', backref='clientes')
+    '''pets = db.relationship('Pet', secondary='pets_has_clientes', lazy='subquery', backref=db.backref('clientes', lazy=True))'''
+    '''pets = db.relationship('Pet', secondary='pets_has_clientes', lazy='subquery', backref=db.backref('clientes', lazy=True))'''
 
     def __repr__(self):
         return f"<Cliente {self.nome}, Endereço {self.endereco}>"
@@ -17,8 +24,41 @@ class Cliente(db.Model):
     def serialize(self):
         return {"id": self.id,
                 "nome": self.nome,
-                "endereco": self.endereco.serialize()}
+                "endereco": self.endereco.serialize(),
+                "telefone": self.telefone.serialize(),
+                #"pets": self.pets.serialize_list()
+                }
 
+class Pet(db.Model):
+    __tablename__ = 'pets'
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(45), nullable=False)
+    raca = db.Column(db.String(45), nullable=False)
+    porte = db.Column(db.String(10), nullable=False)
+    genero = db.Column(db.String(1), nullable=False)
+    especie = db.Column(db.String(15), nullable=False)
+
+
+    #clientes = db.relationship('Cliente', secondary='pets_has_clientes', backref='pets')
+    '''
+    clientes = db.relationship('Cliente', secondary='pets_has_clientes', lazy='subquery',
+        backref=db.backref('pets', lazy=True))'''
+
+    def __repr__(self):
+        return f"<Pet {self.nome}>" #, Cliente {self.cliente.serialize()}
+
+    def serialize(self):
+        return {"id": self.id,
+                "nome": self.nome,
+                "raca": self.raca}
+    
+    
+
+
+pets_has_clientes = db.Table('pets_has_clientes',
+    db.Column('cliente_id', db.Integer, db.ForeignKey('clientes.id'), primary_key=True),
+    db.Column('pet_id', db.Integer, db.ForeignKey('pets.id'), primary_key=True)
+)
 
 class Endereco(db.Model):
     __tablename__ = 'enderecos'
@@ -36,3 +76,19 @@ class Endereco(db.Model):
         return {"id": self.id,
                 "rua": self.rua,
                 "cidade": self.cidade}
+
+
+class Telefone(db.Model):
+    __tablename__ = 'telefones'
+    id = db.Column(db.Integer, primary_key=True)
+    telefone = db.Column(db.String(11), nullable=False, unique=True)
+
+    def __repr__(self):
+        return f'<Telefone {self.telefone}>'
+
+    def serialize(self):
+        return {"id": self.id,
+                "telefone": self.telefone}
+
+
+
